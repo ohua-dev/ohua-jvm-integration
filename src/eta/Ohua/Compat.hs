2b3312e5294a637036c9238314da16077e68e784
Justus Adam <dev@justus.science>
2017-08-22 15:39:26 +0200

    Exporting the compiler function

--- src/eta/Ohua/Compat.hs
+++ src/eta/Ohua/Compat.hs
@@ -23,17 +23,8 @@ import qualified Data.HashMap.Strict as HM
 import Ohua.LensClasses
 import Unsafe.Coerce
 import Ohua.DFGraph
+import Ohua.Compile
 
--- data {-# CLASS "clojure.lang.Symbol" #-} Symbol = Symbol (Object# Symbol) deriving Class
-
--- foreign import java "@static ohua.util.Eta.createSymbol" createSymbol :: String -> String -> Symbol
--- foreign import java "@static ohua.util.Eta.createSymbol" createSymbol1 :: String -> Symbol
-
--- foreign import java "getName" symGetName :: Symbol -> String
--- foreign import java "getNamespace" symGetNamespace :: Symbol -> Maybe String
-
--- data {-# CLASS "clojure.lang.Keyword" #-} Keyword = Keyword (Object# Keyword) deriving (Class, Eq)
--- foreign import java "@static clojure.lang.Keyword.find" createKeyword :: String -> Keyword
 
 instance Show Object where show = fromJString . toString
 
@@ -45,7 +36,8 @@ class NativeConverter a where
 
 type CljExpr = Object
 
-
+not_implemented :: a
+not_implemented = error "This function is not (yet) implemented"
 
 data {-# CLASS "com.ohua.alang.Expr" #-} NExpr = NExpr (Object# NExpr) deriving (Class)
 
@@ -64,6 +56,7 @@ instance NativeConverter Expression where
                     (fromNative $ nLetExprValue let_)
                     (fromNative $ nLetExprBody let_)
             _ -> error "unconvertable"
+    toNative = not_implemented
 
 data {-# CLASS "ohua.alang.Expr$Let" #-} NLetExpr = NLetExpr (Object# NLetExpr) deriving (Class)
 
@@ -91,7 +84,8 @@ instance NativeConverter Assignment where
             (Just dir, _) -> Direct $ fromNative $ nDirectAssignmentBinding dir
             (_, Just destr) -> Destructure $ map fromNative $ fromJava (destr :: NBindingArr)
             _ -> error "unconvertable"
-
+    toNative = not_implemented
+            
 foreign import java "@field body" nLambdaExprBody :: NLambdaExpr -> NExpr
 
 data {-# CLASS "ohua.alang.Assignment" #-} NAssignment = NAssignment (Object# NAssignment) deriving (Class)
@@ -108,6 +102,7 @@ data {-# CLASS "ohua.types.Binding" #-} NBinding = NBinding (Object# NBinding) d
 instance NativeConverter Binding where
     type NativeType Binding = NBinding
     fromNative nbind = Binding (fromJava $ nBindingValue nbind)
+    toNative = not_implemented
 
 foreign import java "@field value" nBindingValue :: NBinding -> JString
 
@@ -126,6 +121,7 @@ instance NativeConverter ResolvedSymbol where
             (_, Just v, _) -> Sf (fromNative $ nSfBindingFnName v) (fmap fromNative $ nSfBindingId v)
             (_, _, Just v) -> Env $ fromNative $ nEnvBindingId v
             _ -> error "unconvertable"
+    toNative = not_implemented
 
 data {-# CLASS "ohua.alang.ResolvedSymbol$Local" #-} NLocalBinding = NLocalBinding (Object# NLocalBinding) deriving Class
 
@@ -148,6 +144,7 @@ data {-# CLASS "ohua.graph.Target" #-} NTarget = NTarget (Object# NTarget) deriv
 instance NativeConverter Target where
     type NativeType Target = NTarget
     toNative (Target id idx) = newNTarget (toNative id) (toJava idx)
+    fromNative = not_implemented
 
 foreign import java "@new" newNTarget :: JInteger -> JInteger -> NTarget
 
@@ -156,6 +153,7 @@ data {-# CLASS "ohua.graph.Graph" #-} NGraph = NGraph (Object# NGraph) deriving 
 instance NativeConverter OutGraph where
     type NativeType OutGraph = NGraph
     toNative (OutGraph ops arcs) = newNGraph (toJava $ map toNative ops) (toJava $ map toNative arcs)
+    fromNative = not_implemented
 
 foreign import java "@new" newNGraph :: NOperatorArr -> NArcArr -> NGraph
 
@@ -164,6 +162,7 @@ data {-# CLASS "ohua.graph.Operator" #-} NOperator = NOperator (Object# NOperato
 instance NativeConverter Operator where
     type NativeType Operator = NOperator
     toNative (Operator id t) = newNOperator (toNative id) (toNative t)
+    fromNative = not_implemented
 
 foreign import java "@new" newNOperator :: JInteger -> NFnName -> NOperator
 
@@ -177,6 +176,7 @@ instance NativeConverter Source where
     type NativeType Source = NSource
     toNative (LocalSource t) = superCast $ newNLocalSource $ toNative t
     toNative (EnvSource e) = superCast $ newNEnvSource $ toNative e
+    fromNative = not_implemented
 
 
 data {-# CLASS "ohua.graph.Source$Local" #-} NLocalSource = NLocalSource (Object# NLocalSource) deriving Class
@@ -194,6 +194,7 @@ data {-# CLASS "ohua.graph.Arc" #-} NArc = NArc (Object# NArc) deriving Class
 instance NativeConverter Arc where
     type NativeType Arc = NArc
     toNative (Arc target source) = newNArc (toNative target) (toNative source)
+    fromNative = not_implemented
 
 foreign import java "@new" newNArc :: NTarget -> NSource -> NArc
 
@@ -206,89 +207,10 @@ data {-# CLASS "ohua.types.FnName" #-} NFnName = NFnName (Object# NFnName) deriv
 instance NativeConverter FnName where
     type NativeType FnName = NFnName
     fromNative fnName = FnName (fromJava $ nFnNameName fnName) (fromJava $ nFnNameNamespace fnName)
+    toNative = not_implemented
 
 foreign import java "@field name" nFnNameName :: NFnName -> JString
 foreign import java "@field namespace" nFnNameNamespace :: NFnName -> JString
-
--- data {-# CLASS "com.ohua.ir.IRFunc" #-} NativeIRFunc = NativeIRFunc (Object# NativeIRFunc) deriving Class
-
--- foreign import java "@new" mkNativeIRFunc :: Object -> Object -> Object -> Object -> NativeIRFunc
--- foreign import java "@field id" getNativeIRFuncId :: NativeIRFunc -> Object
--- foreign import java "@field args" getNativeIRFuncArgs :: NativeIRFunc -> Object
--- foreign import java "@field name" getNativeIRFuncName :: NativeIRFunc -> Object
--- foreign import java "@field return" getNativeIRFuncReturn :: NativeIRFunc -> Object
-
--- instance NativeConverter IRFn where
---     type NativeType IRFn = NativeIRFunc
---     fromNative func = IRFn
---         (fromNative $ unsafeCast $ getNativeIRFuncId func)
---         (fromNative $ unsafeCast $ getNativeIRFuncName func)
---         (fromNative $ unsafeCast $ getNativeIRFuncArgs func)
---         (fromNative $ unsafeCast $ getNativeIRFuncReturn func)
---     toNative (IRFn id_ name_ args ret) = mkNativeIRFunc
---         (superCast $ toNative id_)
---         (superCast $ toNative name_)
---         (superCast $ toNative args)
---         (superCast $ toNative ret)
-
-
--- instance NativeConverter IRGraph where
---     type NativeType IRGraph = Collection (NativeType IRFn)
---     fromNative = map fromNative . fromJava
---     toNative = toJava . map toNative
-
-
--- opIdKW = createKeyword "op-id"
--- typeKW = createKeyword "type"
--- outVarKW = createKeyword "out-var"
-
--- instance NativeConverter CtxtFrame where
---     type NativeType CtxtFrame = Map Keyword Object
---     fromNative nm = CtxtFrame (fromNative $ unsafeCast frameType) (fromNative $ unsafeCast opid) (fromJava $ (unsafeCast outVar :: JInteger))
---       where
---         m = fromJava nm :: [(Keyword, Object)]
---         Just opid = lookup opIdKW m
---         Just frameType = lookup typeKW m
---         Just outVar = lookup outVarKW m
---     toNative frame = toJava
---         [ (opIdKW, superCast $ toNative $ frame^.sourceFn :: Object)
---         , (typeKW, superCast $ toNative $ frame^.frameType)
---         , (outVarKW, superCast $ (toJava $ frame^.outVar :: JInteger))
---         ]
-
--- instance NativeConverter CtxtStack where
---     type NativeType CtxtStack = List (NativeType CtxtFrame)
---     toNative = toJava . map toNative
---     fromNative = map fromNative . fromJava
-
-
--- instance NativeConverter CtxtMap where
---     type NativeType CtxtMap = Map JInteger (NativeType CtxtStack)
---     fromNative = HM.fromList . map (fromNative *** fromNative) . fromJava
---     toNative = toJava . map (toNative *** toNative) . HM.toList
-
--- instance NativeConverter Argument where
---     type NativeType Argument = Object
---     fromNative arg =
---         case safeDowncast arg of
---             Just sym -> ABinding $ fromNative sym
---             Nothing -> AEnv $ unsafeCoerce arg
---     toNative (AEnv o) = unsafeCoerce o
---     toNative (ABinding bnd) = superCast $ toNative bnd
-
--- instance NativeConverter Arguments where
---     type NativeType Arguments = List (NativeType Argument)
---     fromNative = map fromNative . fromJava
---     toNative = toJava . map toNative
-
--- instance NativeConverter FnReturn where
---     type NativeType FnReturn = Object
---     fromNative ret =
---         case safeDowncast ret of
---             Just sym -> Direct $ fromNative sym
---             Nothing -> Destructure $ map fromNative (fromJava $ (unsafeCast ret :: Collection Symbol))
---     toNative (Direct bnd) = superCast $ toNative bnd
---     toNative (Destructure l) = superCast $ (toJava $ map toNative l :: List Symbol)
 
 instance NativeConverter FnId where
     type NativeType FnId = JInteger
@@ -300,12 +222,12 @@ instance NativeConverter HostExpr where
     fromNative = HostExpr . fromJava
     toNative = toJava . unwrapHostExpr
 
--- instance NativeConverter FnName where
---     type NativeType FnName = Symbol
---     fromNative sym = maybe (error "Symbol for fn name must be fully qualified") (`FnName` symGetName sym) (symGetNamespace sym)
---     toNative n = createSymbol (n^.namespace) (n^.name)
 
--- instance NativeConverter Binding where
---     type NativeType Binding = Symbol
---     toNative = createSymbol1 . unBinding
---     fromNative sym = maybe (Binding $ symGetName sym) (const $ error "Symbol for binding must not be qualified") (symGetNamespace sym)
+data {-# CLASS "ohua.Compiler" #-} NCompiler = NCompiler (Object# NCompiler) deriving Class
+
+
+nativeCompile :: NExpr -> Java NCompiler NGraph
+nativeCompile = return . toNative . either error id . compile . fromNative
+
+
+foreign export java "@static ohua.Compiler.compile" nativeCompile :: NExpr -> Java NCompiler NGraph
