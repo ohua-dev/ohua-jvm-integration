f2beb6315806518dab3fbdba393277183e4a104d
Justus Adam <dev@justus.science>
2017-08-29 14:27:50 +0200

    Minmized dummy main example

--- app/DummyMain.hs
+++ app/DummyMain.hs
@@ -1,8 +1,27 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, FlexibleContexts, StandaloneDeriving #-}
 import Ohua.Compat.JVM.Compiler
 import Java
 import qualified Clojure
+import Ohua.Monad
+import Data.Functor.Identity
+import Ohua.Compile
+import Ohua.Compat.JVM.ToALang
+import Ohua.Compat.JVM.Marshal
+import Ohua.DFGraph
 
 
-main = putStrLn . fromJava . toString . nativeCompile =<< Clojure.read "(a b c)"
+deriving instance Show OutGraph
+deriving instance Show Operator
+deriving instance Show Arc
+deriving instance Show Target
+deriving instance Show Source
+
+
+-- main = putStrLn . fromJava . toString . nativeCompile =<< Clojure.read "(a b c)"
+
+-- main = print . either error id . compile . fromNative =<< Clojure.read "(a b c)"
+--   where
+--     compile st = runOhuaT0 (pipeline . fst =<< toALang st) (definedBindings st)
     
+
+main = print =<< runOhuaT0 generateBinding mempty
