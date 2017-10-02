893233dac082e6b7afcac2de8c02f7329303ddc0
Justus Adam <dev@justus.science>
2017-10-02 14:49:21 +0200

    Fixed type in linker interface

--- src/java/ohua/Linker.java
+++ src/java/ohua/Linker.java
@@ -6,8 +6,8 @@ import ohua.types.FnName;
 public interface Linker {
     /**
      * Resolve a stateful function.
-     * Returns the fully qualified version of the potentially unqualified 
+     * Returns the fully qualified version of the potentially unqualified
      * input name or null if no stateful function with this name is in scope.
      */
-    FnName resolve(String name);
+    String resolve(String name);
 }
