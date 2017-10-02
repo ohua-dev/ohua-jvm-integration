21b7cee86986a78e8f725f50e7646535919ccb5e
Justus Adam <dev@justus.science>
2017-10-02 14:48:37 +0200

    Fixed ohua macro arity and :import usage

--- src/clojure/ohua/lang.clj
+++ src/clojure/ohua/lang.clj
@@ -9,8 +9,18 @@
           )
 
 
-(defmacro ohua [code options]
-  (let [graph (ohua.Compiler/compileAndSpliceEnv
-                ohua.link/clj-linker
-                code)]
-    (ohua.Runtime/prepare graph)))
+(defn ohua-fn [code option]
+  (case code
+    :import (do
+              (println "Using the :import version of the ohua macro is deprecated")
+              (apply ohua.link/ohua-require-fn (map (fn [ns_] [ns_ :refer :all]) option)))
+    (let [_ (println code)
+          graph (ohua.Compiler/compileAndSpliceEnv
+                  ohua.link/clj-linker
+                  code)]
+      (ohua.Runtime/prepare graph))))
+
+
+(defmacro ohua
+  ([code] (ohua-fn code {}))
+  ([code options] (ohua-fn code options)))
