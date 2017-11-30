cb433f8ffdac7d82e17c6c56a77fb58450e16c95
Justus Adam <dev@justus.science>
2017-11-30 13:06:55 +0100

    Building test cases for clojure stateful functions

--- test/clojure/ohua/clojure_embedding_test.clj
+++ test/clojure/ohua/clojure_embedding_test.clj
@@ -9,16 +9,36 @@
       (<-ohua
         (- (+ 5 (int 10)) 1)))))
 
+(defn atom? [a]
+  (instance? clojure.lang.Atom a))
+
+
+(defn ^{:init-state '(atom nil)} set-b [a new-val]
+  (reset! a new-val)
+  a)
+
+(deftest explicit-stateful-clojure-functions-test 
+  (is (not (nil? (get :init-state (meta set-b)))))
+  (is (atom? (eval (get :init-state (meta set-b)))))
+  (is
+    (<-ohua 
+      (let [val (set-b 10)]
+        (and
+          (atom? val)
+          (= 10 (deref val))))
+      {:logging :debug})))
 
 (defsfn set-a (atom nil) [a new-val]
   (reset! a new-val)
   a)
 
 (deftest stateful-clojure-functions-test 
+  (is (not (nil? (get :init-state (meta set-a)))))
+  (is (atom? (eval (get :init-state (meta set-a)))))
   (is
     (<-ohua 
       (let [val (set-a 10)]
         (and
-          (instance? clojure.lang.Atom val)
+          (atom? val)
           (= 10 (deref val))))
       {:logging :debug})))
